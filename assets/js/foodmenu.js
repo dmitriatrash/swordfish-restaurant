14ce46b1f72cc1b2617441bafc7f08a39b114ff2
originalzen <dmitriatrash@gmail.com>
2018-08-08 22:48:09 -0400

    Update Files

--- assets/js/foodmenu.js
+++ assets/js/foodmenu.js
@@ -1,33 +1,36 @@
 /*
 Created by: Dmitri Atrash
 Course Name: ICT-4510, Adv Website Design & Mgmt (Summer 2018)
-Final Project
+Assignment 8
 This script retrieves JSON data using a jQuery Ajax GET request, stores it in the browser's localStorage, and
 retrieves it to display a pie chart using ChartJS.
  */
 
 //Function to get data and place it in localStorage
 function getMenuData () {
-	let url = 'assets/chartdata/piechart-data.json';
-	$.get(url, function (data) {
-		window.localStorage.setItem('menuData', JSON.stringify(data));
-	});
+    let url = 'assets/menu.json';
+    $.get(url, function (data) {
+        window.localStorage.setItem('menuData', JSON.stringify(data));
+    });
 }
 
-//Display Menu Data
-function displayMenuData() {
-
-	let data = JSON.parse(window.localStorage.getItem('pieChartData')); //Turn data into JS object
-	let ctx = document.getElementById("myPieChart").getContext("2d");
-	let myPieChart = new Chart(ctx).Pie(data, options);
+//Display table
+function displayMenu() {
+    let data = JSON.parse(window.localStorage.getItem('menuData')); //Retrieve Data
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + data[i].Name + "</td>");
+        tr.append("<td>" + data[i].Price + "</td>");
+        tr.append("<td>" + data[i].Category + "</td>");
+        tr.append("<td>" + data[i].Description + "</td>");
+        $('#foodMenu').append(tr);
+    }
 }
 
 //Retrieve data
-getPieChartData();
+getMenuData();
 
 //Display chart when button is clicked
-$(document).ready(function() {
-    $('#displayButton').click(function() {
-        displayPieChart();
-    });
-});
+displayMenu(this);
+
